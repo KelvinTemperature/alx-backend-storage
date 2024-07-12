ef371a04ba6466330ccb30480dd0e589e885b3dd
Kelvin <koolkt10@gmail.com>
2024-07-12 15:04:49 +0000

    task 1&2

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
---create a Table user
---with attributes id, email, name
-DROP TABLE IF EXIST user;
-CREATE TABLE user(
+-- create a Table user
+-- with attributes id, email, name
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,9 @@
---create a Table user
---with attributes id, email, name, country
-DROP TABLE IF EXIST user;
-CREATE TABLE user(
+-- create a Table user
+-- with attributes id, email, name, country
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255)
-	country CHAR(2) NOT NULL DEFAULT 'US' CHECK(country IN ('US', 'CO', 'TN'))
+	name VARCHAR(255),
+	country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
 );
