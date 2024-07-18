86b5fd5472ea14431683150a0850c0b2862f22a5
Kelvin <koolkt10@gmail.com>
2024-07-17 22:37:57 -0700

    web done

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -4,19 +4,8 @@ Main file
 """
 import redis
 
-Cache = __import__('exercise').Cache
+Cache = __import__('web').get_page
 
-cache = Cache()
+page = Cache('http://slowwly.robertomurray.co.uk')
 
-s1 = cache.store("first")
-print(s1)
-s2 = cache.store("secont")
-print(s2)
-s3 = cache.store("third")
-print(s3)
-
-inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
-outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
-
-print("inputs: {}".format(inputs))
-print("outputs: {}".format(outputs))
+print(page.data_catcher)
